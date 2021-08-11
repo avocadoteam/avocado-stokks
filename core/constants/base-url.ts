5a50f4db1751e404da68b3c923ae3475aff76239
konst <konstantin.mikheev@icord.cz>
2021-08-11 07:04:58 +0200

    fq

--- core/constants/base-url.ts
+++ core/constants/base-url.ts
@@ -1,2 +1,2 @@
-// export const baseUrl = 'https://stokks.herokuapp.com/api/';
-export const baseUrl = 'http://192.168.0.14:3000/api/';
+export const baseUrl = 'https://stokks.herokuapp.com/api/';
+// export const baseUrl = 'http://192.168.0.14:3000/api/';
