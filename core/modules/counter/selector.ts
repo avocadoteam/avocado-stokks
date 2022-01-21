e6b678102345216c4857ed5eb501fd650e5f1226
Konstantin Mikheev <konstantin.mikheev@seznam.cz>
2022-01-21 13:45:10 +0100

    removed unused

--- core/modules/counter/selector.ts
+++
@@ -1,6 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { State } from '../../store/root-reducer';
-
-const counterState = (state: State) => state.counter;
-
-export const getCounterValue = createSelector(counterState, c => c.value);
--- core/store/root-reducer.ts
+++ core/store/root-reducer.ts
@@ -1,13 +1,11 @@
-import { combineReducers, PayloadAction } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
 import { authReducer } from 'core/modules/auth/reducer';
 import { stockApi } from 'core/modules/stock/query';
 import { stockReducer } from 'core/modules/stock/reducer';
 import { urlParserApi } from 'core/modules/url-parser/query';
 import { userApi } from 'core/modules/user/query';
-import { counterReducer } from '../modules/counter/reducer';
 
 export const rootReducer = combineReducers({
-  counter: counterReducer,
   auth: authReducer,
   stock: stockReducer,
   [urlParserApi.reducerPath]: urlParserApi.reducer,
@@ -16,7 +14,6 @@ export const rootReducer = combineReducers({
 });
 
 export type State = ReturnType<typeof rootReducer>;
-export type AllActions = PayloadAction;
 
 declare module 'react-redux' {
   export interface DefaultRootState extends State {}
