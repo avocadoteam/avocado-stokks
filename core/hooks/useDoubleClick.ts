02d3bcf4eb6c36da892130795fdcf12df596d64e
XtereO <71135845+XtereO@users.noreply.github.com>
2022-02-12 19:57:05 +0300

    'formatingHooks'

--- core/hooks/useDoubleClick.ts
+++ core/hooks/useDoubleClick.ts
@@ -1,19 +1,19 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 export const useDoubleClick = (callback: () => void) => {
-    const [clickCount, setClickCount] = useState(0)
-    let timer:number;
+  const [clickCount, setClickCount] = useState(0);
+  let timer: number;
 
-    return () => {
-        setClickCount(prev => (prev + 1))
-        if (clickCount === 2) {
-            setClickCount(0)
-            clearTimeout(timer)
-            callback()
-        } else {
-            timer = setTimeout(() => {
-                setClickCount(0)
-            }, 3000)
-        }
+  return () => {
+    setClickCount(prev => prev + 1);
+    if (clickCount === 2) {
+      setClickCount(0);
+      clearTimeout(timer);
+      callback();
+    } else {
+      timer = setTimeout(() => {
+        setClickCount(0);
+      }, 3000);
     }
-}
+  };
+};
