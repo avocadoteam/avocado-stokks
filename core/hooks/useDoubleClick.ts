de39c3f2173cafd4a022d699c1ad866d8d8beeb1
XtereO <71135845+XtereO@users.noreply.github.com>
2022-02-15 20:00:47 +0300

    refactoring-part3

--- core/hooks/useDoubleClick.ts
+++ core/hooks/useDoubleClick.ts
@@ -1,17 +1,15 @@
 import { useState } from 'react';
+import { useTimeout } from './useTimeout';
 
 export const useDoubleClick = (callback: () => void) => {
   const [clickCount, setClickCount] = useState(0);
+  useTimeout(callback, 3000, clickCount);
 
   return () => {
     setClickCount(prev => prev + 1);
     if (clickCount === 2) {
       setClickCount(0);
       callback();
-    } else {
-      setTimeout(() => {
-        setClickCount(0);
-      }, 3000);
     }
   };
 };
--- ui/Skeletons/SkeletonStockBanner/SkeletonRegularMarketBanner.tsx
+++ ui/Skeletons/SkeletonStockBanner/SkeletonRegularMarketBanner.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Box, Flex } from 'native-base';
 import { StyleSheet } from 'react-native';
 import { SkeletonBox } from '../SkeletonBox';
 
 export const SkeletonRegularMarketBanner = React.memo(({}) => {
-  const leftSideJSX = [1, 2, 3].map(l => <Srmv key={`rmvs${l}`} />);
-  const rightSideJSX = [4, 5, 6].map(r => <Srmv key={`rmvs${r}`} />);
-
   return (
     <Flex marginTop={8} direction="row" style={styles.mainBox}>
-      <Box style={styles.leftSide}>{leftSideJSX}</Box>
-      <Box style={styles.rightSide}>{rightSideJSX}</Box>
+      <Box style={styles.leftSide}>
+        <SkeletonSide />
+      </Box>
+      <Box style={styles.rightSide}>
+        <SkeletonSide />
+      </Box>
     </Flex>
   );
 });
@@ -27,11 +28,19 @@ const styles = StyleSheet.create({
   },
 });
 
-const Srmv = React.memo(({}) => {
-  return (
-    <Box marginTop={4}>
-      <SkeletonBox width={57} height={14} />
-      <SkeletonBox width={88} height={18} marginTop={10} />
-    </Box>
-  );
+const SkeletonSide = React.memo(({}) => {
+  const getItems = useMemo(() => {
+    let elements = [];
+    for (let i = 0; i < 3; i++) {
+      elements.push(
+        <Box key={i} marginTop={4}>
+          <SkeletonBox width={57} height={14} />
+          <SkeletonBox width={88} height={18} marginTop={10} />
+        </Box>,
+      );
+    }
+    return elements;
+  }, []);
+
+  return <>{getItems}</>;
 });
