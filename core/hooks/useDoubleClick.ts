fe08fb1e625b100f0c25df027cc644aa3bb24c8c
XtereO <71135845+XtereO@users.noreply.github.com>
2022-02-16 21:57:12 +0300

    Change useTimeout in useDoubleClick

--- core/hooks/useDoubleClick.ts
+++ core/hooks/useDoubleClick.ts
@@ -3,7 +3,7 @@ import { useTimeout } from './useTimeout';
 
 export const useDoubleClick = (callback: () => void) => {
   const [clickCount, setClickCount] = useState(0);
-  useTimeout(callback, 3000, () => setClickCount(0));
+  useTimeout(() => setClickCount(0), 3000, [clickCount]);
 
   return () => {
     setClickCount(prev => prev + 1);
