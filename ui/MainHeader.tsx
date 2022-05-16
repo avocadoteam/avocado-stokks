0ec95416e4abc9f59ae1b8eff153da61c5853978
XtereO <71135845+XtereO@users.noreply.github.com>
2022-05-16 21:37:08 +0300

    Settings page UI

--- ui/MainHeader.tsx
+++ ui/MainHeader.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
-import { IconButton, Icon, HStack, Heading, useTheme } from 'native-base';
+import { IconButton, Icon, HStack, Heading, useTheme, Flex } from 'native-base';
 import { AntDesign } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { NavigationModal, NavigationScreen } from 'core/models';
@@ -7,6 +7,7 @@ import { Pressable } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { modalActions } from 'core/modules/modal/reducer';
 import { useDoubleClick } from 'core/hooks/useDoubleClick';
+import { SettingsOutlineIcon } from './icons/SettingsOutlineIcon';
 
 type Props = {
   showWelcome: boolean;
@@ -23,17 +24,23 @@ export const MainHeader = memo<Props>(({ showWelcome }) => {
   const openSearch = React.useCallback(() => {
     navigation.navigate(NavigationScreen.Search);
   }, []);
+  const openSettings = React.useCallback(() => {
+    navigation.navigate(NavigationScreen.Settings);
+  }, []);
 
   return (
     <HStack mt={12} py={2} px={6} justifyContent="space-between" alignItems="center">
       <Pressable onPress={doubleClickTitleHandler}>
         <Heading color={colors.heading}>{showWelcome ? 'Welcome' : 'Stokks'}</Heading>
       </Pressable>
-      <IconButton
-        onPress={openSearch}
-        variant="unstyled"
-        icon={<Icon size="sm" as={<AntDesign name="search1" />} color={colors.primary[100]} />}
-      />
+      <Flex flexDirection="row">
+        <IconButton
+          onPress={openSearch}
+          variant="unstyled"
+          icon={<Icon size="sm" as={<AntDesign name="search1" />} color={colors.primary[100]} />}
+        />
+        <IconButton onPress={openSettings} variant="unstyled" icon={<SettingsOutlineIcon />} />
+      </Flex>
     </HStack>
   );
 });
