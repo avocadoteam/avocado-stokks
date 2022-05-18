fecca5034c78e79d8d25d9009aa9b85f1937dacf
XtereO <71135845+XtereO@users.noreply.github.com>
2022-05-18 14:22:36 +0300

    Notifications banner

--- ui/SettingBanner/NotificationsBanner.tsx
+++ ui/SettingBanner/NotificationsBanner.tsx
@@ -1,21 +1,44 @@
+import { isNotificationAllowed } from 'core/modules/notifications/selectors';
 import { Box, Heading, useTheme } from 'native-base';
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Separator } from 'ui/atoms/Separator';
+import { CheckCircleIcon } from 'ui/icons/CheckCircleIcon';
 import { NotificationOutlineIcon } from 'ui/icons/NotificationOutlineIcon';
-import { NotificationOutlineImportantIcon } from 'ui/icons/NotificationOutlineImportantIcon';
 import { NotificationOutlineOffIcon } from 'ui/icons/NotificationOutlineOffIcon';
 import { SettingCell } from 'ui/SettingCell';
+import * as Notifications from 'expo-notifications';
+import { notificationActions } from 'core/modules/notifications/reducer';
 
 export const NotificationsBanner = memo(() => {
   const { colors } = useTheme();
+  const dispatch = useDispatch();
+  const isNotificationTurnedOn = useSelector(isNotificationAllowed);
+  const handleOffNotification = () => {
+    dispatch(notificationActions.allowNotifications(false));
+  };
+  const handleOnNotification = () => {
+    dispatch(notificationActions.allowNotifications(true));
+  };
+
   return (
     <Box mt={3}>
       <Heading size={'sm'} color={colors.heading}>
         Notifications
       </Heading>
       <Box>
-        <SettingCell before={<NotificationOutlineIcon width={20} height={22} color={colors.headingSmall} />}>On</SettingCell>
-        <SettingCell before={<NotificationOutlineOffIcon width={24} height={24} color={colors.headingSmall} />}>
+        <SettingCell
+          onPress={handleOnNotification}
+          after={isNotificationTurnedOn && <CheckCircleIcon />}
+          before={<NotificationOutlineIcon width={20} height={22} color={colors.headingSmall} />}
+        >
+          On
+        </SettingCell>
+        <SettingCell
+          onPress={handleOffNotification}
+          after={!isNotificationTurnedOn && <CheckCircleIcon />}
+          before={<NotificationOutlineOffIcon width={24} height={24} color={colors.headingSmall} />}
+        >
           Off
         </SettingCell>
       </Box>
