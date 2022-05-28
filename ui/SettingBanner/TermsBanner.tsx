58fdf0a2121d5e06deb3937a54b76e99b2482f58
XtereO <71135845+XtereO@users.noreply.github.com>
2022-05-28 15:37:12 +0300

    Added link to TermsBanner

--- ui/SettingBanner/TermsBanner.tsx
+++ ui/SettingBanner/TermsBanner.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, useTheme } from 'native-base';
+import { Box, Heading, Link, useTheme } from 'native-base';
 import React, { memo } from 'react';
 import { Separator } from 'ui/atoms/Separator';
 import { BlockedOutlineIcon } from 'ui/icons/BlockedOutlineIcon';
@@ -13,8 +13,24 @@ export const TermsBanner = memo(() => {
         Terms
       </Heading>
       <Box>
-        <SettingCell before={<BlockedOutlineIcon />}>Privacy Policy</SettingCell>
-        <SettingCell before={<PaperOutlineIcon />}>Terms of Use</SettingCell>
+        <SettingCell before={<BlockedOutlineIcon />}>
+          <Link
+            isUnderlined={false}
+            _text={{ color: colors.headingSmall, fontWeight: 'bold', fontSize: 16 }}
+            href={'https://pages.flycricket.io/stokks-0/privacy.html'}
+          >
+            Privacy Policy
+          </Link>
+        </SettingCell>
+        <SettingCell before={<PaperOutlineIcon />}>
+          <Link
+            isUnderlined={false}
+            _text={{ color: colors.headingSmall, fontWeight: 'bold', fontSize: 16 }}
+            href={'https://pages.flycricket.io/stokks-0/privacy.html'}
+          >
+            Terms of Use
+          </Link>
+        </SettingCell>
       </Box>
       <Separator />
     </Box>
